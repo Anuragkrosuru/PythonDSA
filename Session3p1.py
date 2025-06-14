bb42af51ab0a1dada173133500fd042af040c860
Anuragkrosuru <anuragkrosuru@gmail.com>
2025-06-14 13:44:35 -0400

    Add files via upload

---
+++ Session3p1.py
@@ -0,0 +1,9 @@
+def count_words(words):
+    word_count = {}
+    for word in words:
+        if word in word_count:
+            word_count[word] +=1
+        else:
+            word_count[word] = 1
+    return word_count
+
